573ab9da332ad88451e11fd2bb98ebae945d2aec
Candice Ariel <candicewangsa@gmail.com>
2023-08-11 00:31:44 +0700

    translate function

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const lngs = {
+  en: { nativeName: 'English' },
+  zh: { nativeName: '中文' }
+};
+
 function Header() {
   const navigate = useNavigate();
 
-  const setLang = (l) => {
-    navigate('/', { state: { lang: l } })
+  const changeLang = (lng) => {
+    navigate('/', { state: { lang: lng } })
   };
 
   return (
     <div className="c-header">
-      <button onClick={() => {setLang("en");}}>English</button>
-      <button onClick={() => {setLang("chi");}}>中文</button>
+      {/* <button onClick={() => {setLang("en");}}>English</button>
+      <button onClick={() => {setLang("chi");}}>中文</button> */}
+      {Object.keys(lngs).map((lng) => (
+        <button key={lng} onClick={() => changeLang(lng)}>
+          {lngs[lng].nativeName}
+        </button>
+      ))}
     </div>
   );
 }
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect } from "react";
 import {useLocation} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function Home() {
+  const { t, i18n } = useTranslation()
   const location = useLocation();
   const locationState = location.state;
   const lang = locationState ? locationState.lang : '';
 
   useEffect(() => {
-    // Update the document title using the browser API
+    setLang(lang)
     console.log(lang)
   });
 
+  const setLang = (l) => {
+    i18n.changeLanguage(l)
+  };
+
   return (
     <div className="o-home">
+      <h1>{t('hello')}</h1>
     </div>
   );
 }
